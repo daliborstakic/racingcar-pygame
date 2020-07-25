136b235dc41984d62f87ae297e69601a7d7a281e
daliborstakic <stakicdalibor64@gmail.com>
2020-07-25 17:28:44 +0200

    Functionality for increasing enemy move speed. Message displays on end. Game is replayable.

--- main.py
+++ main.py
@@ -18,12 +18,17 @@ pygame.display.set_icon(PLAYER_IMG)
 
 # Colors
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Fonts
+END_FONT = pygame.font.SysFont('arial', 40)
 
 # Clock
 clock = pygame.time.Clock()
 FPS = 60
 
 
+# Abstract class for car
 class Car(ABC):
     @abstractmethod
     def move(self):
@@ -34,11 +39,13 @@ class Car(ABC):
         pass
 
 
+# Class enemy inherits the Car class
 class Enemy(Car):
     def __init__(self):
         self.x = 800
         self.y = random.choice([124, 234])
-        self.moving_speed = 5
+
+    moving_speed = 0
 
     def draw(self):
         win.blit(ENEMY_IMG, (self.x, self.y))
@@ -47,6 +54,7 @@ class Enemy(Car):
         self.x -= self.moving_speed
 
 
+# Class player inherits the Car class
 class Player(Car):
     def __init__(self):
         self.x = 50
@@ -60,26 +68,26 @@ class Player(Car):
 
         if keys[pygame.K_DOWN]:
             self.y = 234
-
         elif keys[pygame.K_UP]:
             self.y = 124
 
 
+# Spawning an enemy depending on the condition
 def enemy_spawn(enemy):
-    if enemy.x == WIDTH // 2:
-        enemies.append(Enemy())
-
     if enemy.x <= -ENEMY_IMG.get_width():
+        enemies.append(Enemy())
         enemies.pop(0)
 
 
+# Checking for collisions
 def has_collided(enemy):
-    if enemy.x <= player.x <= enemy.x + ENEMY_IMG.get_width() and player.y == enemy.y:
+    if enemy.x <= player.x + PLAYER_IMG.get_width() <= enemy.x + ENEMY_IMG.get_width() and player.y == enemy.y:
         return True
 
     return False
 
 
+# Drawing the screen
 def render():
     win.fill(WHITE)
     win.blit(BACKGROUND, (0, 0))
@@ -90,32 +98,59 @@ def render():
 
     pygame.display.update()
 
-def display_message(subject):
-    pass
 
+# Displaying end message
+def display_message(subject):
+    pygame.time.delay(500)
+    win.fill(WHITE)
+    display_text = END_FONT.render(subject, 1, BLACK)
+    win.blit(display_text, ((WIDTH - display_text.get_width()) // 2, (HEIGHT - display_text.get_height()) // 2))
+    pygame.display.update()
+    pygame.time.delay(2000)
+
+
+# Main function
 def main():
     global player, enemies
 
+    # Getting the ticks from the beginning of the game
+    start_ticks = pygame.time.get_ticks()
+    last_second = 0
+
     run = True
 
+    # Setting initial moving speed
+    Enemy.moving_speed = 5
+
     player = Player()
-    enemies = [Enemy()]
+    enemies = [Enemy()]  # Array of enemies
 
     while run:
         clock.tick(FPS)
+
+        # Seconds from the start of the game
+        seconds = int((pygame.time.get_ticks() - start_ticks) / 1000)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-            player.move()
+            player.move()  # Moving the player
 
+        # Looping through the enemies
         for enemy in enemies:
             enemy.move()
             enemy_spawn(enemy)
 
+            # Ending the game if it has collided
             if has_collided(enemy):
+                display_message("You lost!")
                 run = False
+
+        # Increasing enemy move speed
+        if seconds % 5 == 0 and seconds != last_second:
+            Enemy.moving_speed += 1
+            last_second = seconds  # Making sure that it executes once every second
 
         render()
 
